648c9bf55eaf32535eafa5d838e5c9ce37594454
namanrana <47075510+namanrana16@users.noreply.github.com>
2020-07-06 13:57:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/redx/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.redx
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity,SignIn::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
