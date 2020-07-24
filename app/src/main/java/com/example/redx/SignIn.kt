4841da8a1ef1b17020606846fd7d35503d65d7ea
namanrana <47075510+namanrana16@users.noreply.github.com>
2020-07-24 13:25:00 +0530

    4

--- app/src/main/java/com/example/redx/SignIn.kt
+++ app/src/main/java/com/example/redx/SignIn.kt
@@ -3,6 +3,7 @@ package com.example.redx
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 
 import android.widget.Button
 import android.widget.EditText
@@ -11,10 +12,12 @@ import com.example.redx.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.util.regex.Pattern
 
 
 lateinit var email :EditText
 lateinit var password :EditText
+
 private lateinit var auth: FirebaseAuth
 private lateinit var binding: ActivitySignInBinding
    class SignIn : AppCompatActivity()  {
@@ -27,8 +30,10 @@ private lateinit var binding: ActivitySignInBinding
         button.setOnClickListener {
             val intent= Intent(this,SignUp::class.java)
             startActivity(intent)
-        }
 
+
+            
+        }
 
 
         auth = Firebase.auth
@@ -37,6 +42,180 @@ private lateinit var binding: ActivitySignInBinding
 
 
        }
+
+
+
+class Validator {
+
+    companion object {
+
+        // Default validation messages
+        private val PASSWORD_POLICY = """Password should be minimum 8 characters long,
+            |should contain at least one capital letter,
+            |at least one small letter,
+            |at least one number and
+            |at least one special character among ~!@#$%^&*()-_=+|[]{};:'\",<.>/?""".trimMargin()
+
+//        private const val NAME_VALIDATION_MSG = "Enter a valid name"
+        private const val EMAIL_VALIDATION_MSG = "Enter a valid email address"
+//        private const val PHONE_VALIDATION_MSG = "Enter a valid phone number"
+
+        /**
+         * Retrieve string data from the parameter.
+         * @param data - can be EditText or String
+         * @return - String extracted from EditText or data if its data type is Strin.
+         */
+        private fun getText(data: Any): String {
+            var str = ""
+            if (data is EditText) {
+                str = data.text.toString()
+            } else if (data is String) {
+                str = data
+            }
+            return str
+        }
+
+        /**
+         * Checks if the name is valid.
+         * @param data - can be EditText or String
+         * @param updateUI - if true and if data is EditText, the function sets error to the EditText or its TextInputLayout
+         * @return - true if the name is valid.
+         */
+//        fun isValidName(data: Any, updateUI: Boolean = true): Boolean {
+//            val str = getText(data)
+//            val valid = str.trim().length > 2
+//
+//             Set error if required
+//             if (updateUI) {
+//                val error: String? = if (valid) null else NAME_VALIDATION_MSG
+//                setError(data, error)
+//            }
+//
+//            return valid
+//        }
+
+        /**
+         * Checks if the email is valid.
+         * @param data - can be EditText or String
+         * @param updateUI - if true and if data is EditText, the function sets error to the EditText or its TextInputLayout
+         * @return - true if the email is valid.
+         */
+        fun isValidEmail(data: Any, updateUI: Boolean = true): Boolean {
+            val str = getText(data)
+            val valid = Patterns.EMAIL_ADDRESS.matcher(str).matches()
+
+            // Set error if required
+            if (updateUI) {
+                val error: String? = if (valid) null else EMAIL_VALIDATION_MSG
+                setError(data, error)
+            }
+
+            return valid
+        }
+
+        /**
+         * Checks if the phone is valid.
+         * @param data - can be EditText or String
+         * @param updateUI - if true and if data is EditText, the function sets error to the EditText or its TextInputLayout
+         * @return - true if the phone is valid.
+         */
+   /*     fun isValidPhone(data: Any, updateUI: Boolean = true): Boolean {
+            val str = getText(data)
+            val valid = Patterns.PHONE.matcher(str).matches()
+
+            // Set error if required
+            if (updateUI) {
+                val error: String? = if (valid) null else PHONE_VALIDATION_MSG
+                setError(data, error)
+            }
+
+            return valid
+        }
+
+        */
+
+        /**
+         * Checks if the password is valid as per the following password policy.
+         * Password should be minimum minimum 8 characters long.
+         * Password should contain at least one number.
+         * Password should contain at least one capital letter.
+         * Password should contain at least one small letter.
+         * Password should contain at least one special character.
+         * Allowed special characters: "~!@#$%^&*()-_=+|/,."';:{}[]<>?"
+         *
+         * @param data - can be EditText or String
+         * @param updateUI - if true and if data is EditText, the function sets error to the EditText or its TextInputLayout
+         * @return - true if the password is valid as per the password policy.
+         */
+        fun isValidPassword(data: Any, updateUI: Boolean = true): Boolean {
+            val str = getText(data)
+            var valid = true
+
+            // Password policy check
+            // Password should be minimum minimum 8 characters long
+            if (str.length < 8) {
+                valid = false
+            }
+            // Password should contain at least one number
+            var exp = ".*[0-9].*"
+            var pattern = Pattern.compile(exp, Pattern.CASE_INSENSITIVE)
+            var matcher = pattern.matcher(str)
+            if (!matcher.matches()) {
+                valid = false
+            }
+
+            // Password should contain at least one capital letter
+            exp = ".*[A-Z].*"
+            pattern = Pattern.compile(exp)
+            matcher = pattern.matcher(str)
+            if (!matcher.matches()) {
+                valid = false
+            }
+
+            // Password should contain at least one small letter
+            exp = ".*[a-z].*"
+            pattern = Pattern.compile(exp)
+            matcher = pattern.matcher(str)
+            if (!matcher.matches()) {
+                valid = false
+            }
+
+            // Password should contain at least one special character
+            // Allowed special characters : "~!@#$%^&*()-_=+|/,."';:{}[]<>?"
+            exp = ".*[~!@#\$%\\^&*()\\-_=+\\|\\[{\\]};:'\",<.>/?].*"
+            pattern = Pattern.compile(exp)
+            matcher = pattern.matcher(str)
+            if (!matcher.matches()) {
+                valid = false
+            }
+
+            // Set error if required
+            if (updateUI) {
+                val error: String? = if (valid) null else PASSWORD_POLICY
+                setError(data, error)
+            }
+
+            return valid
+        }
+
+        /**
+         * Sets error on EditText or TextInputLayout of the EditText.
+         * @param data - Should be EditText
+         * @param error - Message to be shown as error, can be null if no error is to be set
+         */
+        private fun setError(data: Any, error: String?) {
+            if (data is EditText) {
+                if (data.parent.parent is EditText) {
+                    (data.parent.parent as EditText).error = error
+                } else {
+                    data.error = error
+                }
+            }
+        }
+
+    }
+
+}
 
 
 
--- app/src/main/java/com/example/redx/SignUp.kt
+++ app/src/main/java/com/example/redx/SignUp.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 
 import android.widget.Button
 import com.example.redx.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+private lateinit var auth: FirebaseAuth
 
 class SignUp : AppCompatActivity() {
 private lateinit var binding3 : ActivitySignUpBinding
@@ -21,12 +26,15 @@ private lateinit var binding3 : ActivitySignUpBinding
         }
 
 
+        auth = Firebase.auth
 
 
 
     }
 
-
-
 }
+
+
+
+
 
